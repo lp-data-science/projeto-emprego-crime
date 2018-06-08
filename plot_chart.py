4e5005fc6adb6cf2903ed1e5d1865897cba27fe2
Daniel Carvalho <danieljosecarvalho@gmail.com>
2018-06-08 09:26:15 -0300

    - Tratando o dataframe para agupar por data e tipo de crime; - Plotar o gráfico das quantidades de crime por tipo e por data;

--- plot_chart.py
+++ plot_chart.py
@@ -8,10 +8,6 @@ def createDF(dicionario):
     return dataf
 
 
-def concatDF(df1, df2):
-    return pd.concat([df1, df2], ignore_index=True)
-
-
 r = requests.get('http://206.81.2.122/empregos/v1/crimes/1')
 
 res_json = r.json()
@@ -20,8 +16,10 @@ lista_dicionarios = res_json['message']
 
 dataframe = createDF(lista_dicionarios)
 
-dataframe_mes = dataframe[dataframe.data == "2010-09-01T00:00:00.000Z"]
+fig, ax = pl.subplots(figsize=(15, 7))
 
-pl.bar(dataframe_mes["data"], dataframe_mes["quantidade"])
+dataframe_processed = dataframe.groupby(["data", "tipo_crime"]).count()['quantidade'].unstack().plot(ax=ax)
+
+print(dataframe.groupby(["data", "tipo_crime"]).mean())
 
 pl.show()
