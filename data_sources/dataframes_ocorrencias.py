c5fa7f42b27c86869c05419ad98db030a4af7a3d
Daniel Carvalho <danieljosecarvalho@gmail.com>
2018-07-01 20:00:12 -0300

    Correção do enconding ao ler os csvs de ocorrências; Alguns testes para capturar valores das ocorrências; Testes de plotagem de gráfico do tipo heatmap em população;

--- data_sources/dataframes_ocorrencias.py
+++ data_sources/dataframes_ocorrencias.py
@@ -13,7 +13,7 @@ data_frame_list = {}
 def fillDataFramesOcorrencias(file):
     global data_frame_list
 
-    f = open(file, 'r', encoding='iso-8859-15')
+    f = open(file, 'r', encoding='utf-8')
 
     df = pd.read_csv(f, sep=';')
 
@@ -26,3 +26,11 @@ def fillDataFramesOcorrencias(file):
     f.close()
 
     return data_frame_list
+
+for file in files:
+    a = fillDataFramesOcorrencias(file)
+b = data_frame_list['ocorrenciasmun-brasil2014'].where(data_frame_list['ocorrenciasmun-brasil2014']['Tipo Crime'] == 'Estupro')
+c = b.where(b['Sigla UF'] == 'PB')
+print(b['Tipo Crime'].dropna())
+print(b['PC-Qtde Ocorrências'].dropna())
+print(c.dropna())
