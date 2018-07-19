c86f0fdeb7d398f29a5d3b2214fc4f4f30c8d42e
joaovitor7 <vitorpeu@hotmail.com>
2018-07-19 20:32:06 -0300

    Atualização em empregos

--- src/data_sources/dataframes_empregos.py
+++ src/data_sources/dataframes_empregos.py
@@ -12,7 +12,8 @@ empregos_dir = join(current_dir, "dados_empregos/*.txt")
 files_names = glob.glob(empregos_dir)
 data_frame_dict = {}
 
-result_key = []
+result_keyIBGE = []
+result_keySexo = []
 
 def fillDataFramesEmpregos(file):
     global data_frame_dict
@@ -42,41 +43,84 @@ data_frames = getDataFramesEmpregos()
 
 
 def getKey(dictElement):
-    global result_key
+    global result_keyIBGE
+    global  result_keySexo
     key, value = dictElement
 
     if key[0:10] == 'Setor IBGE' and key[-10:] == ' Desligado':
-         result_key.append(key)
+         result_keyIBGE.append(key)
+    elif key[0:4] == 'Sexo' and key[-10:] == ' Desligado':
+        result_keySexo.append(key)
 
-handles = []
-colors = ['mediumblue', 'red', 'lime', 'purple', 'grey', 'black', 'darkolivegreen']
-ax = None
 
-def plotByKey(key):
+
+
+
+
+
+handlesIBGE = []
+colorsIBGE = ['mediumblue', 'red', 'lime', 'purple', 'grey', 'black', 'darkolivegreen']
+axIBGE = None
+
+def plotByKeySetorIBGE(key):
     global data_frames
-    global ax
-    global handles
-    global colors
+    global axIBGE
+    global handlesIBGE
+    global colorsIBGE
 
     data_frame = data_frames[key]
     setor = key[11:].split('-')
     setor = setor[0]
     data_frame = data_frame[data_frame.estado_ibge == 26]
-    color = colors.pop(0)
-    if ax == None:
-        ax = data_frame.plot(color=color, x='ano', y='valor')
+    color = colorsIBGE.pop(0)
+    if axIBGE == None:
+
+        axIBGE = data_frame.plot(color=color, x='ano', y='valor')
     else:
-        ax = data_frame.plot(color=color, x='ano', y='valor', ax=ax)
+        axIBGE = data_frame.plot(color=color, x='ano', y='valor', ax=axIBGE)
 
     patch = mpatches.Patch(color=color, label=setor)
-    handles.append(patch)
+    handlesIBGE.append(patch)
+
+
+listDataframeSexo = []
+
+def getDataframesBySexo(key):
+    global data_frames
+    global axSexo
+    global handlesSexo
+    global colorsSexo
+
+    data_frame = data_frames[key]
+    listDataframeSexo.append(data_frame)
+
+
+def plotDesempregoGeral():
+    data_frame1 = listDataframeSexo[0]
+    data_frame2 = listDataframeSexo[1]
+
+    geral = pd.DataFrame({'ano': data_frame1.ano, 'estado_ibge': data_frame1.estado_ibge,
+                          'valor': data_frame1.valor + data_frame2.valor})
+    data_frame = geral[geral.estado_ibge == 26]
+
+    data_frame.plot(color='red', x='ano', y='valor')
+
 
 
 list(map(getKey, data_frames.items()))
+list(map(getDataframesBySexo, result_keySexo))
+plotDesempregoGeral()
 
-list(map(plotByKey, result_key))
+list(map(plotByKeySetorIBGE, result_keyIBGE))
 
-plt.legend(title='Setor', handles=handles)
+
+
+
+
+
+
+
+plt.legend(title='Setor', handles=handlesIBGE)
 plt.xlabel("ano")
 plt.ylabel("população")
 plt.show()
